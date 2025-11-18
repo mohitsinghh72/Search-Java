b140653f2e33c024d41eba9c226d27daf2d03eb3
Mohit Singh <mohitsinghh72@gmail.com>
2025-11-18 23:35:04 +0530

    Insertion Sort

---
+++ InsertionSort.java
@@ -0,0 +1,26 @@
+//Insertion sort
+//It is a stable sort
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {23,1,54,21,3,55};
+        Sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    static void Sort(int[] arr){
+        for(int i = 0;i<arr.length-1;i++){
+            for(int j = i+1;j>0;j--){
+                if(arr[j]<arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }else{
+                    break;
+                }
+            }
+        }
+    }
+}
